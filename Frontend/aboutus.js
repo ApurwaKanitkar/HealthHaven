8e58166355394b2fede74b67a6ba87544c6a6f48
apoorv0208 <apoorvanasane@gmail.com>
2024-04-01 13:32:44 +0530

    frontend stage 1

---
+++ Frontend/aboutus.js
@@ -0,0 +1,7 @@
+// main.js
+
+// Update phone number dynamically
+const phoneNumber = document.getElementById('phone');
+phoneNumber.addEventListener('click', () => {
+    alert('Please call during business hours (9 AM - 5 PM).');
+});
